b968669d40ee931fa03affb9ea078ebf75a5a9c7
Anirban Kar <anirban.k59@gmail.com>
2023-07-23 18:26:20 +0530

    delete test file

--- automate_boring_stuff/test.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(numbers):
-    sum = 0
-    for number in numbers:
-        sum += number
-    return sum
-
-total = add_numbers([1,2,3])
-print("The sum of these numbers is " + str(total) + ".")
