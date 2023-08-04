916fa0527495f84cfcf7d9b07b2c40731141f31e
Anirban Kar <anirban.k59@gmail.com>
2023-08-04 18:54:58 +0530

    new program added and aesthetical changes to rpcls program

---
+++ CS50/extensions.py
@@ -0,0 +1,19 @@
+def file_extensions(file_name):
+    suffixes = {
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.png': 'image/png',
+        '.pdf': 'application/pdf',
+        '.txt': 'text/plain',
+        '.zip': 'application/zip'
+    }
+
+    file_extensions = file_name.split(".")[-1].lower()
+    return suffixes.get("." + file_extensions, "application/octet-stream")
+file_name = input("What is the name of the file: ").lower().strip()
+if "." not in file_name:
+    file_ext = "application/octet-stream"
+else:
+    file_ext = file_extensions(file_name)
+print(file_ext)
