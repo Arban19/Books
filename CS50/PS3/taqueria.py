c2aa382009346e797330900aa76d2ec8300e8740
Anirban Kar <anirban.k59@gmail.com>
2023-08-24 13:42:30 +0530

    add new program

--- CS50/PS3/taqueria.py
+++ CS50/PS3/taqueria.py
@@ -1,4 +1,4 @@
-item = {
+rate_chart = {
     "Baja Taco": 4.00,
     "Burrito": 7.50,
     "Bowl": 8.50,
@@ -9,3 +9,19 @@ item = {
     "Taco": 3.00,
     "Tortilla Salad": 8.00
 }
+
+def main():
+    order = []
+    order_cost = 0.0
+    try:
+        while True:
+            item = input("Item: ").title().strip()
+            if item in rate_chart:
+                order_cost += rate_chart[item]
+                print(f"Total: ${order_cost:.2f}")
+            else:
+                continue
+    except EOFError:
+        pass
+
+main()
