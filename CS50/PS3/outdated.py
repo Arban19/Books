d468e2606b075242c8912bda5f90d035e614518a
Anirban Kar <anirban.k59@gmail.com>
2023-08-24 23:22:31 +0530

    remove redundancies

--- CS50/PS3/outdated.py
+++ CS50/PS3/outdated.py
@@ -1,33 +1,25 @@
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+months =  ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 while True:
-        date = input("Date: ").strip()
-        if "/" in date:
-            month, day, year = date.split("/")
-        elif "," in date:
-            date = date.replace(",","")
-            month, day, year = date.split(" ")
-            if month in months:
-                month = months.index(month) + 1
         try:
+            date = input("Date: ").strip()
+            if "/" not in date and "," not in date:
+                raise ValueError
+            if "/" in date:
+                month, day, year = date.split("/")
+            else:
+                date = date.replace(",","")
+                parts = date.split(" ")
+                month, day, year = parts
+                if len(parts) != 3:
+                    raise ValueError
+                if month in months:
+                    month = months.index(month) + 1
             if int(month) > 12 or int(day) > 31:
                 raise ValueError
             else:
                 break
-        except (ValueError, AttributeError, NameError, KeyError):
+        except ValueError:
             pass
 
 print(f"{year}-{int(month):02}-{int(day):02}")
