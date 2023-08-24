9209b8fe8cb2b692ef8ca518c3947294a8423d57
Anirban Kar <anirban.k59@gmail.com>
2023-08-24 22:13:27 +0530

    remove bugs

--- CS50/PS3/outdated.py
+++ CS50/PS3/outdated.py
@@ -14,20 +14,23 @@ months = [
 ]
 
 while True:
-    try:
         date = input("Date: ")
         if "/" in date:
             month, day, year = date.split("/")
+            day = int(day)
+            month = int(month)
         elif "," in date:
             date = date.replace(",","")
             month, day, year = date.split(" ")
+            day = int(day)
             if month in months:
                 month = months.index(month) + 1
-        if int(month) > 12 and int(date) > 31:
-            raise ValueError
-        else:
-            break
-    except (ValueError, AttributeError, NameError, KeyError):
-        pass
+        try:
+            if int(month) > 12 or int(day) > 31:
+                raise ValueError
+            else:
+                break
+        except (ValueError, AttributeError, NameError, KeyError):
+            pass
 
 print(f"{year}-{int(month):02}-{int(day):02}")
