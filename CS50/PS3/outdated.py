270e8d67f19bc6dab65540c9b173eb1415934378
Anirban Kar <anirban.k59@gmail.com>
2023-08-24 23:23:06 +0530

    aesthetical changes

--- CS50/PS3/outdated.py
+++ CS50/PS3/outdated.py
@@ -2,9 +2,11 @@ months =  ["January","February","March","April","May","June","July","August","Se
 
 while True:
         try:
+
             date = input("Date: ").strip()
             if "/" not in date and "," not in date:
                 raise ValueError
+
             if "/" in date:
                 month, day, year = date.split("/")
             else:
@@ -15,10 +17,12 @@ while True:
                     raise ValueError
                 if month in months:
                     month = months.index(month) + 1
+
             if int(month) > 12 or int(day) > 31:
                 raise ValueError
             else:
                 break
+
         except ValueError:
             pass
 
