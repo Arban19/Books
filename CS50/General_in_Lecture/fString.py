882b361a951aff8168fbbe42fec72a6ced8e620e
Anirban Kar <anirban.k59@gmail.com>
2023-08-25 17:09:28 +0530

    add new program

--- CS50/General_in_Lecture/fString.py
+++ CS50/General_in_Lecture/fString.py
@@ -2,3 +2,11 @@ x = float(input("What's x? "))
 y = float(input("What's y? "))
 z = round(x + y)
 print(f"{z:,}")
+
+
+text = "Arban"
+
+print(f"{text}")
+print(f"{text:#<20}")
+print(f"{text:_>20}")
+print(f"{text:$^20}")
