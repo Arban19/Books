315b6a09986a0a89d0e68eb84de7c2c6d6cb4237
Anirban Kar <anirban.k59@gmail.com>
2023-08-25 16:13:25 +0530

    streamline the program

--- CS50/General_in_Lecture/name_con.py
+++ CS50/General_in_Lecture/name_con.py
@@ -1,7 +1,6 @@
 import sys
 
 if len(sys.argv) < 2 or len(sys.argv) > 2:
-    z = input("What is your name? ")
-    print(f"Hello. Pleased to meet you, {z}.")
-else:
-    print(f"Hello. Pleased to meet you, {sys.argv[1]}.")
+    sys.exit("Proper format not adhered to")
+
+print(f"Hello. Pleased to meet you, {sys.argv[1]}.")
