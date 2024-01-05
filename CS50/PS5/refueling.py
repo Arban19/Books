88445785a2cb5a38641d8db2bf1b3abd324d8f10
Anirban Kar <anirban.k59@gmail.com>
2024-01-05 17:45:00 +0530

    add new programs

--- CS50/PS5/refueling.py
+++ CS50/PS5/refueling.py
@@ -1,11 +1,28 @@
 def main():
-    pass
+    fraction = input("Fraction: ")
+    result = convert(fraction)
+    print(gauge(result))
 
 def convert(fraction):
-    pass
+    try:
+        num, den  = fraction.split("/")
+        x = int(num)
+        y = int(den)
+        percentage = (x/y)*100
+        if percentage <= 100:
+            return percentage
+        else:
+            raise ValueError
+    except (ValueError, ZeroDivisionError):
+        raise
 
 def gauge(percentage):
-    pass
+    if percentage <= 1:
+        return "E"
+    if percentage >= 99:
+        return "F"
+    else:
+        return str(round(percentage)) + "%"
 
 if __name__ == "__main__":
     main()
---
+++ CS50/PS5/test_refueling.py
@@ -0,0 +1,26 @@
+import pytest
+
+from fuel import convert, gauge
+
+def main():
+    test_correct_input()
+    test_zero_division()
+    test_value_error()
+
+def test_convert():
+    assert convert("1/4") == 25
+    assert convert("1/100") == 1
+    assert convert("99/100") == 99
+
+def test_gauge():
+    assert gauge(25) == "25%"
+    assert gauge(1) == "E"
+    assert gauge(99) == "F"
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        assert convert("1/0")
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        assert convert("cat/dog")
