ed880ca67f1e8c80521d2edf2f93749c7d7b6b86
Anirban Kar <anirban.k59@gmail.com>
2024-01-06 16:38:25 +0530

    remove errors and improve code

--- CS50/PS2/vanity.py
+++ CS50/PS2/vanity.py
@@ -1,30 +1,26 @@
+def is_valid(plate):
+    return (2<= len(plate) <= 6) and plate[:2].isalpha() and number_end(plate) and not_zero(plate) and plate.isalnum()
+
+def number_end(plate):
+    for index, char in enumerate(plate):
+        if char.isdigit():
+            return plate[index:].isdigit()
+    return True
+
+def not_zero(plate):
+    for i in plate:
+        if i.isdigit():
+            if i == "0":
+                return False
+            else:
+                break
+    return True
+
 def main():
-    s = input("Plate: ")
-    if is_valid(s):
+    plate = input("Plate: ")
+    if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
-def not_zero(s):
-    i = 0
-    while i < len(s):
-        if s[i].isdigit():
-            if s[i] == "0":
-                return False
-            else:
-                break
-        i += 1
-    return True
-
-def does_not_end_with_str(s):
-    if len(s) > 2:
-        return not s[-1].isalpha()
-    else:
-        return True
-
-def is_valid(s):
-    return (2 <= len(s) <= 6) and s[:2].isalpha() and not_zero(s) and s.isalnum() and does_not_end_with_str(s)
-
-
-if __name__ == "__main__":
-    main()
+main()
