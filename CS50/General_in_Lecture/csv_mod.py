4f405836c37df9a01181b81520c6c8576cfc256e
Anirban Kar <anirban.k59@gmail.com>
2024-02-15 19:24:19 +0530

    csv module-- csv.reader

---
+++ CS50/General_in_Lecture/csv_mod.py
@@ -0,0 +1,23 @@
+# import csv
+
+# students = []
+
+# with open("students_address.csv") as file:
+#     reader = csv.reader(file)
+#     for name, home in reader:
+#         students.append({"name": name, "home": home})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is in {student['home']}")
+
+import csv
+
+students = []
+
+with open("students_address.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
