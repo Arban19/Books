5c35db5a750f5759ecf3f47b3412a3f63e5f4ec7
Anirban Kar <anirban.k59@gmail.com>
2024-02-20 09:18:09 +0530

    refactor count lines problem from problem set 6

--- CS50/PS6/count_lines.py
+++ CS50/PS6/count_lines.py
@@ -1,16 +1,13 @@
 import sys
 
 def main():
-    no_of_lines = 0
     if check_command_line_argument():
         try:
             files = open(sys.argv[1],"r")
             lines = files.readlines()
         except FileNotFoundError:
             sys.exit("File does not exist")
-        for line in lines:
-            if not check_if_comment_or_blank(line):
-                no_of_lines += 1
+        no_of_lines = sum(1 for line in lines if not (line.isspace() or line.lstrip().startswith("#")))
         print(no_of_lines)
 
 def check_command_line_argument():
@@ -21,11 +18,6 @@ def check_command_line_argument():
     if ".py" not in sys.argv[1]:
         sys.exit("Not a python file")
     return True
-
-def check_if_comment_or_blank(line):
-    if line.isspace() or line.lstrip().startswith("#"):
-        return True
-    return False
 
 if __name__ == "__main__":
     main()
