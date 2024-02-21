d8643417bb12b52f082088d006affd882f35637c
Anirban Kar <anirban.k59@gmail.com>
2024-02-21 16:59:15 +0530

    add new program

--- CS50/General_in_Lecture/csv_dict.py
+++ CS50/General_in_Lecture/csv_dict.py
@@ -5,7 +5,7 @@ students = []
 with open("students_address.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        students.append({"name": row["name"], "home": ["home"]})
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    print(f"{student['name']} is in {student['home']}")
